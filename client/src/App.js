972da769fdb4d88be494555d2307c006eff55da4
PJ <peterjoe2005@gmail.com>
2024-09-29 12:54:18 -0400

    added alternating values for the buttons

--- client/src/App.js
+++ client/src/App.js
@@ -2,30 +2,38 @@ import { useState } from 'react';
 import './App.css';
 
 export default function App() {
+  const [turn, setTurn] = useState('X');
+
   return (
     <div className="App">
       <h1>Tic Tac Toe Game</h1>
       <div className='grid-container'>
-        <div><Box /></div>
-        <div><Box /></div>
-        <div><Box /></div>
-        <div><Box /></div>
-        <div><Box /></div>
-        <div><Box /></div>
-        <div><Box /></div>
-        <div><Box /></div>
-        <div><Box /></div>
+        <div><Box turn={turn} setTurn={setTurn}/></div>
+        <div><Box turn={turn} setTurn={setTurn}/></div>
+        <div><Box turn={turn} setTurn={setTurn}/></div>
+        <div><Box turn={turn} setTurn={setTurn}/></div>
+        <div><Box turn={turn} setTurn={setTurn}/></div>
+        <div><Box turn={turn} setTurn={setTurn}/></div>
+        <div><Box turn={turn} setTurn={setTurn}/></div>
+        <div><Box turn={turn} setTurn={setTurn}/></div>
+        <div><Box turn={turn} setTurn={setTurn}/></div>
       </div>
     </div>
   );
 }
 
-function Box() {
-  //initialize state with useState
+function Box({turn, setTurn}) {
   const [value, setValue] = useState('');
 
   function onClick(){
-    setValue('X'); //update state
+    if(value == ''){
+      setValue(turn);
+      if(turn == 'X'){
+        setTurn('O')
+      } else {
+        setTurn('X')
+      }
+    }
   }
 
   return (
