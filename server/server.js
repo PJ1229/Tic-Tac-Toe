8807226227fce47a3c52496922ab8f1e78d1e167
PJ <peterjoe2005@gmail.com>
2024-09-29 22:23:35 -0400

    fix path to build folder

--- server/server.js
+++ server/server.js
@@ -2,15 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+// Serve static files from the client/build folder
+app.use(express.static(path.join(__dirname, '../client/build')));
 
-// Handles any requests that don't match the ones above
+// Catch-all handler: for any request, send back the index.html file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
